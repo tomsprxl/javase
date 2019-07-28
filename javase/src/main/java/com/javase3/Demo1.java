abee04211311c94ecdfef6c305e09922ccaeb760
xuliang <18656756681@163.com>
2019-07-28 11:19:30 +0800

    add

--- javase/src/main/java/com/javase3/Demo1.java
+++ javase/src/main/java/com/javase3/Demo1.java
@@ -10,5 +10,6 @@ public class Demo1 {
         int i1=0b1100;
         System.out.println(i);
         System.out.println(i1);
+        //git remote add origin git@github.com:tomsprxl/javase.git
     }
 }
