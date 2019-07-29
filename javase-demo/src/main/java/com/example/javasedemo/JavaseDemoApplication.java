c5562ba6de97730532e960e3c3469838ce5aa9cf
xuliang <18656756681@163.com>
2019-07-29 21:59:41 +0800

    add javase-demo

---
+++ javase-demo/src/main/java/com/example/javasedemo/JavaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.javasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaseDemoApplication.class, args);
+    }
+
+}
